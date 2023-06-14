981579fda3eed4d7e68795b3a494d898e5465563
changju <changju7857@gmail.com>
2023-06-14 12:12:47 +0900

    # 5. 추가버튼 및 할 일 목록 렌더링 완료

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -5,10 +5,35 @@ import TodoInput from './TodoInput';
 import './scss/TodoTemplate.scss';
 
 const TodoTemplate = () => {
+
+  // 서버에 할 일 목록(json)을 요청(fetch)해서 받아와야 함.
+  const todos = [
+    {
+      id : 1,
+      title : '아침 산책하기',
+      done : true
+    },
+    {
+      id : 2,
+      title : '오늘 주간 신문 읽기',
+      done : true
+    },
+    {
+      id : 3,
+      title : '샌드위치 사먹기',
+      done : false
+    },
+    {
+      id : 4,
+      title : '리액트 복습하기',
+      done : true
+    }
+  ];
+
   return (
     <div className='TodoTemplate'>
         <TodoHeader />
-        <TodoMain />
+        <TodoMain todoList = {todos} />
         <TodoInput />
     </div>
   );
