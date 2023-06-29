e1abf06dbeb728c123fb410e9481cda46f0daf1b
changju <changju7857@gmail.com>
2023-06-29 11:59:32 +0900

    # 17. 프로필 이미지 등록 및 렌더링 완료

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -5,11 +5,19 @@ import './header.css';
 import { Link, useNavigate } from 'react-router-dom';
 import { isLogin, getLoginUserInfo } from '../../util/login-utils';
 import AuthContext from '../../util/AuthContext';
+import { API_BASE_URL, USER } from '../../config/host-config';
 
 
 const Header = () => {
 
+    const profileRequestURL = `${API_BASE_URL}${USER}/load-profile`;
+
     const redirection = useNavigate();
+
+    
+    // 프로필 이미지 url 상태 변수
+    const [profileUrl, setProfileUrl] = useState(null);
+
 
     // AuthContext에서 로그인 상태와 onLogout 함수를 가져옵니다.
     const { isLoggedIn, onLogout, userName } = useContext(AuthContext);
@@ -26,9 +34,30 @@ const Header = () => {
     }
     
 
-    // useEffect(() => {
-    //     setUserInfo(getLoginUserInfo());
-    // }, []);
+    const fetchProfileImage = async() => {
+        const res = await fetch(profileRequestURL, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + localStorage.getItem('ACCESS_TOKEN') }
+        });
+
+        if(res.status === 200) {
+            // 서버에서는 직렬화된 이미지가 응답된다.
+            const profileBlob = await res.blob();
+            // 해당 이미지를 imgUrl로 변경
+            const imgUrl = window.URL.createObjectURL(profileBlob);
+            setProfileUrl(imgUrl);
+        } else {
+            const err = await res.text();
+            setProfileUrl(null);
+        }
+    }
+
+
+    useEffect(() => {
+
+        fetchProfileImage();
+
+    }, [isLoggedIn])
 
 
 
@@ -54,7 +83,19 @@ const Header = () => {
                                     : '오늘'
                                 }
                                 의 할 일
-                            </Typography>   
+                            </Typography>
+                            {isLoggedIn && 
+                            <img 
+                                src={profileUrl || require('../../assets/img/anonymous.jpg')}
+                                alt='프사'
+                                style={{
+                                    marginLeft: 20,
+                                    width: 80,
+                                    height: 80,
+                                    borderRadius: '50%'
+                                }}
+                            />
+                            }   
                         </div>
                     </Grid>
 
