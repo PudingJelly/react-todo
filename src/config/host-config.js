28e4d09a1b1e7e71ca9a08db60f17d858c867aab
changju <changju7857@gmail.com>
2023-07-07 14:31:11 +0900

    # 20. AWS 배포 설정

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -5,10 +5,10 @@ const clientHostName = window.location.hostname;
 let backEndHostName; // 백엔드 서버 호스트 이름
 
 if(clientHostName === 'localhost') { // 개발 중
-    // backEndHostName = 'http://localhost:8181';
+    backEndHostName = 'http://localhost:8181';
+    // backEndHostName = 'http://13.125.12.230';
+} else if(clientHostName === 'my-todoapp-7857.s3-website.ap-northeast-2.amazonaws.com') { // 배포해서 서비스 중
     backEndHostName = 'http://13.125.12.230';
-} else if(clientHostName === 'spring.com') { // 배포해서 서비스 중
-    backEndHostName = 'https://api.spring.com';
 }
 
 export const API_BASE_URL = backEndHostName;
